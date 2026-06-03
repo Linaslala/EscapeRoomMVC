7cec16aaff001fa77e0fd992658614b0a936dc37
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-06-03 14:14:55 +0200

    QuestionViewModel created

---
+++ EscapeRoomMVC/Controllers/QuestionController.cs
@@ -0,0 +1,56 @@
+﻿using EscapeRoomMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscapeRoomMVC.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public List<QuestionViewModel> Questions { get; set; } = new List<QuestionViewModel>();
+
+        public IActionResult Index()
+        {
+            Questions.Add(new QuestionViewModel
+            {
+                Id = 1,
+                Title = "Riddle one",
+                Description = "Den har inget lock inte ingen botten, men kan ändå hålla kött, blod och ben på samma gång.",
+                Answer = "Ringen"
+            });
+
+            Questions.Add(new QuestionViewModel
+            {
+                Id = 2,
+                Title = "Riddle two",
+                Description = "Gissar du mig har du gissat fel: gissar du fel har du i alla fall gissat rätt.",
+                Answer = "Fel"
+            });
+
+            Questions.Add(new QuestionViewModel
+            {
+                Id = 3,
+                Title = "Riddle three",
+                Description = "Det simmade tio fiskar i en stängd tank. Två av dem sjönk till botten, fyra simmade iväg och tre dog. Hur många fanns kvar?",
+                Answer = "Tio"
+            });
+
+            Questions.Add(new QuestionViewModel
+            {
+                Id = 4,
+                Title = "Riddle four",
+                Description = "Vad är det för farkost som läses likadant framifrån som bakifrån?",
+                Answer = "Kajak"
+            });
+
+            Questions.Add(new QuestionViewModel
+            {
+                Id = 5,
+                Title = "Riddle five",
+                Description = "Två ingångar till huset, men först när man är ute med fötterna är man riktigt inne?",
+                Answer = "Byxorna"
+            });
+
+
+            return View(Questions);
+        }
+    }
+}
---
+++ EscapeRoomMVC/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EscapeRoomMVC.ViewModels
+{
+    public class QuestionViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
